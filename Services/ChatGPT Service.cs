18ed4e8d7d0aecee962fa58267df320b1bf4c46a
nebula <nebula@orbitalsolutions.ca>
2023-08-03 14:50:01 -0300

    Refactored code, and added support for GPT4

--- Services/ChatGPT Service.cs
+++ Services/ChatGPT Service.cs
@@ -9,40 +9,59 @@ internal static class ChatGPTApi
     internal const string GPT3ModelLarge = "gpt-3.5-turbo-16k";
     internal const string GPT4Model = "gpt-4";
     internal const string GPT4ModelLarge = "gpt-4-32k";
+
     internal static async Task<string?> AskQuestionAsync(string question, string apiKey, string model = GPT3Model)
     {
-        using var http = new HttpClient();
-        http.Timeout = TimeSpan.FromSeconds(666);
-        http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
+        using var http = CreateHttpClient(apiKey);
+        var requestBody = CreateRequestBody(question, model);
 
-        var requestBody = new
-        {
-            model,
-            messages = new[]
-            {
-                new 
-                {
-                    role = "user",
-                    content = question
-                }
-            },
-            temperature = 0
-        };
-
-        var httpResult = await http.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestBody);
-        var httpResponse = await httpResult.Content.ReadAsStringAsync();
+        var httpResponse = await PostQuestionAsync(http, requestBody);
         if (httpResponse.Contains("context_length_exceeded"))
         {
-            requestBody = new
-            {
-                model = model == GPT3Model ? GPT3ModelLarge : GPT4ModelLarge,
-                requestBody.messages,
-                requestBody.temperature
-            };
-            httpResult = await http.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestBody);
-            httpResponse = await httpResult.Content.ReadAsStringAsync();
+            requestBody = UpdateRequestBodyWithLargeModel(requestBody, model);
+            httpResponse = await PostQuestionAsync(http, requestBody);
         }
+
+        return ExtractContentFromResponse(httpResponse);
+    }
+
+    private static HttpClient CreateHttpClient(string apiKey)
+    {
+        var http = new HttpClient { Timeout = TimeSpan.FromSeconds(666) };
+        http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
+        return http;
+    }
+
+    private static object CreateRequestBody(string question, string model)
+    {
+        return new
+        {
+            model,
+            messages = new[] { new { role = "user", content = question } },
+            temperature = 0
+        };
+    }
+
+    private static async Task<string> PostQuestionAsync(HttpClient http, object requestBody)
+    {
+        var httpResult = await http.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestBody);
+        return await httpResult.Content.ReadAsStringAsync();
+    }
+
+    private static object UpdateRequestBodyWithLargeModel(object requestBody, string model)
+    {
+        return new
+        {
+            model = model == GPT3Model ? GPT3ModelLarge : GPT4ModelLarge,
+            messages = requestBody.GetType().GetProperty("messages")?.GetValue(requestBody),
+            temperature = requestBody.GetType().GetProperty("temperature")?.GetValue(requestBody)
+        };
+    }
+
+    private static string? ExtractContentFromResponse(string httpResponse)
+    {
         var jsonResponse = JsonConvert.DeserializeObject<Models.ChatGPTDTO>(httpResponse);
         return jsonResponse?.choices[0].message.content;
     }
 }
+
--- Services/Command Service.cs
+++ Services/Command Service.cs
@@ -2,53 +2,105 @@
 
 public class CommandService
 {
+    private string apiKey;
+    private string gptVersion = ChatGPTApi.GPT3Model;
+
     public async Task StartAsync()
     {
-        Console.WriteLine("Please enter your OpenAI API key: ");
-        var apiKey = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(apiKey))
-        {
-            Console.WriteLine("Error, API key cannot be empty.");
-            Environment.Exit(0);
-        }
+        GetApiKey();
+        SelectGptVersion();
         Console.Clear();
         Console.WriteLine("Please enter a command :)");
 
         while (true)
         {
-            string? input = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(input))
-                input = "";
-            switch (input.ToLower())
-            {
-                case "ask-question":
-                    Console.WriteLine("What is your question?");
-                    var question = Console.ReadLine();
-                    if (string.IsNullOrWhiteSpace(question))
-                    {
-                        Console.WriteLine("Error, question cannot be empty.");
-                        break;
-                    }
-                    var answer = await ChatGPTApi.AskQuestionAsync(question, apiKey);
-                    if (string.IsNullOrWhiteSpace(answer))
-                    {
-                        Console.WriteLine("Error, answer cannot be empty.");
-                        break;
-                    }
-                    Console.WriteLine(answer);
-                    await File.WriteAllLinesAsync(Environment.CurrentDirectory + $@"\question-{DateTime.Now.ToFileTime()}.txt", new[] { question, answer });
-                    break;
-                case "quit":
-                    Console.WriteLine("Goodbye!");
-                    Environment.Exit(0);
-                    break;
-                case "help":
-                    Console.WriteLine("\nask-question\nhelp\nquit\n");
-                    break;
-                default:
-                    Console.WriteLine("Error, command doesn't exist, please try again. Type 'Help' to view the available commands.");
-                    break;
-            }
+            await ProcessCommandAsync();
         }
+    }
+
+    private void GetApiKey()
+    {
+        Console.WriteLine("Please enter your OpenAI API key: ");
+        apiKey = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(apiKey))
+        {
+            Console.WriteLine("Error, API key cannot be empty.");
+            Environment.Exit(0);
+        }
+    }
+
+    private void SelectGptVersion()
+    {
+        Console.WriteLine("Would you like to use GPT3.5 or GPT4? GPT3.5 is the default and is cheaper, but GPT4 is more powerful and requires special access to use.");
+        Console.WriteLine("Type '3.5' or '4' to select a version.");
+        gptVersion = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(gptVersion))
+        {
+            Console.WriteLine("Error, GPT version cannot be empty.");
+            Environment.Exit(0);
+        }
+
+        switch (gptVersion)
+        {
+            case "3.5":
+                gptVersion = ChatGPTApi.GPT3Model;
+                break;
+            case "4":
+                gptVersion = ChatGPTApi.GPT4Model;
+                break;
+            default:
+                Console.WriteLine("Error, GPT version must be either '3.5' or '4'.");
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+    private async Task ProcessCommandAsync()
+    {
+        string? input = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(input))
+            input = "";
+
+        switch (input.ToLower())
+        {
+            case "ask-question":
+                await AskQuestionAsync();
+                break;
+            case "quit":
+                Console.WriteLine("Goodbye!");
+                Environment.Exit(0);
+                break;
+            case "help":
+                Console.WriteLine("\nask-question\nhelp\nquit\n");
+                break;
+            default:
+                Console.WriteLine("Error, command doesn't exist, please try again. Type 'Help' to view the available commands.");
+                break;
+        }
+    }
+
+    private async Task AskQuestionAsync()
+    {
+        Console.WriteLine("What is your question?");
+        var question = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(question))
+        {
+            Console.WriteLine("Error, question cannot be empty.");
+            return;
+        }
+
+        var answer = await ChatGPTApi.AskQuestionAsync(question, apiKey, gptVersion);
+
+        if (string.IsNullOrWhiteSpace(answer))
+        {
+            Console.WriteLine("Error, answer cannot be empty.");
+            return;
+        }
+
+        Console.WriteLine(answer);
+        await File.WriteAllLinesAsync(Environment.CurrentDirectory + $@"\question-{DateTime.Now.ToFileTime()}.txt", new[] { question, answer });
     }
 }
